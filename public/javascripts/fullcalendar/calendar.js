4bdd510ce2cac1e9fc6be2341858fdf45d759f98
Bruno Costa <brunovasquescosta@gmail.com>
2019-07-19 00:52:54 +0100

    Tested specs to get array of data from api to load events

--- public/javascripts/fullcalendar/calendar.js
+++ public/javascripts/fullcalendar/calendar.js
@@ -1,14 +1,23 @@
-var calendar
 document.addEventListener('DOMContentLoaded', function() {
-  var calendarEl = document.getElementById('calendar');
-  calendar = new FullCalendar.Calendar(calendarEl, {
-  	plugins:['dayGrid'],
-	events: '/calendar',
-   failure: function() {
-        alert('there was an error while fetching events!');
-      }
-  });
-  calendar.render();
+  $.get("/relatorios/reservas/2019/08/35",function(data){
+  	let reservas=[]
+  	var rows=data.dados.rows
+  	rows.forEach(function(row){
+  		reservas.push({
+  			title: row.nome,
+  			start: new Date(row.inicio).toLocaleDateString("ko-KR",{ year: 'numeric', month: '2-digit', day: '2-digit' }).replace(/. /g,"-").replace(".",""),
+  			end: new Date(row.fim).toLocaleDateString("ko-KR",{ year: 'numeric', month: '2-digit', day: '2-digit' }).replace(/. /g,"-").replace(".","")
+  		})
+  	})
+  	console.log(reservas)
+	var calendarEl =  document.getElementById('calendar');
+	calendar = new FullCalendar.Calendar(calendarEl, {
+	  plugins:['dayGrid'],
+	  events: reservas,
+		color: "black"
+	});
+	calendar.render();
+  })	
 
 
 });
