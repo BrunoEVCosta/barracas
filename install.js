fb08ecc72b36403b6d5e89dd0bfcd3e4ec816eb8
Bruno Costa <brunocosta@itqb.unl.pt>
2020-07-02 12:29:47 +0100

    Sets the install script to alter the password for first entry in Pessoa

--- install.js
+++ install.js
@@ -1,12 +1,19 @@
 const setPassword = require('./components/barracas/controller/setPassword')
+const prompt = require('password-prompt')
+
+let password = prompt('password: ')
 
 //createUser
 
 let id=1
-let password=""
 
-let options={id,password}
-setPassword(options).then(data=>{
-  console.log("lklklk")
+
+password.then(password=>{
+  let options={id,password}
+  setPassword(options).then(data=>{
+    console.log(`Password alterada para o utilizador ${data.nome} (${data.email}).`)
+    process.exit(0)
+  })  
 })
+
 
