3a5862777c5c55cdc2e8026d9f1cf467aa184f12
Bruno Costa <brunovasquescosta@gmail.com>
2023-06-12 15:16:01 +0100

    Updates project to get dependencies with webpack

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # barracas
 Gestao de espaços 
+
+
+### Building dependencies
+
+```bash
+npm run build
+```
+
+## Setting new master Password
+To set a new master password i.e. for user.id==1 run:
+```bash
+node install.js
+```
+
+## TODO
+- Popper might not be required by using v.5 of bootstrap
+- update google auth
+- install schema layout
+- config bootstrap v5
---
+++ src/index.js
@@ -0,0 +1,20 @@
+//const wp=require(".././webpack.config")
+//console.log(wp)
+import Vue from 'vue/dist/vue.esm.browser';
+import {VueSelect} from "vue-select";
+
+function insertScripts(names) {
+    let collection=[]
+    for(let name of names) {
+        let element = document.createElement('script');
+        element.src = "/webpack/"+name+".js"
+        document.body.appendChild(element)
+    }
+}
+//let names=webpack.entry.map(pkg=>pkg.filename)
+let names=["popper","bootstrap","vue","canvas-datagrid",'cookieconsent']
+insertScripts(names);
+
+window.Vue = Vue
+window.VueSelect=VueSelect
+Object.keys(buildingblocks).forEach(key=>window[key]=buildingblocks[key])
