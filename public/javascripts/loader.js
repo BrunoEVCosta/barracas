ad852e6fb80cc6da33be4e13ffa37367b04887f4
Bruno Costa <brunovasquescosta@gmail.com>
2023-06-13 15:48:02 +0100

    Sets the viewpoint to the size of the users screen

--- public/javascripts/loader.js
+++ public/javascripts/loader.js
@@ -6,10 +6,20 @@ let url
 if (paths[0]==="install"){
     url="/javascripts/installer/init.js";
 }
-
+if (paths[0]==="admin"){
+    url="/javascripts/index/init.js";
+}
+if(paths[0]===""){
+    url="/javascripts/index/init.js"
+}
 
 var script = document.createElement('script');
 if(url !== undefined){
     script.src = url;
     scriptTarget.parentNode.insertBefore(script, scriptTarget);
 }
+
+
+currentWidth=$('meta[name|="viewport"]').attr("content")
+displayWidth=currentWidth.replace(/width=\d+/,"width="+window.outerWidth)
+$('meta[name|="viewport"]').attr("content",displayWidth)
