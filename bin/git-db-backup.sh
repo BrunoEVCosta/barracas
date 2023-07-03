0f0f9f94097fa6eb6e0b327214914865bf400140
Bruno Costa <brunovasquescosta@gmail.com>
2023-07-03 16:53:37 +0100

    Adds changes to daily DB backup

--- bin/git-db-backup.sh
+++ bin/git-db-backup.sh
@@ -3,13 +3,14 @@
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-cd ../Utilities
+cd ${DIR}/../Utilities
+
+git checkout DB-backup
 read -s -p "Password: " password
 ./backupDB.sh brunocosta barracas
 #Requires password fill in
 
 cd ../SQL
-git checkout DB-backup
-git add SQL/LATEST_dump.sql
+git add LATEST_dump.sql
 git commit -m "Periodic Backup of database"
 git checkout master
